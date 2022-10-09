da5d572aab2eb5f0357fffd5ba5c9012a5ce0a44
TylerUnderwood <tylergrantunderwood@gmail.com>
2022-10-09 01:07:56 -0500

    feat: add button selector

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,95 +1,113 @@
 <script setup>
 import Banner from "@/components/Banner.vue";
 import Section from "@/components/Section.vue";
-import Box from "@/components/Box.vue";
+import { nextTick } from "vue";
+
+const buttons = {
+    type: [
+        "primary",
+        "border",
+        "stealth",
+    ],
+    state: [
+        "active",
+        "visited",
+        "disabled",
+    ],
+    size: [
+        "tall",
+        "normal",
+        "thin",
+        "tiny",
+    ],
+    shape: [
+        "stout",
+        "square",
+        "full",
+    ],
+    scheme: [
+        "warn",
+        "error",
+    ],
+}
+
+</script>
+
+<script>
+export default {
+    methods: {
+        selectClass: ( selector, element ) => {
+            selector.addEventListener("change", (event) => {
+                const options = event.target.options;
+
+                for ( let i=0; i<options.length; i++) {
+                    if ( options[i].value === "" ) continue;
+
+                    if ( options[i].selected ) {
+                        element.classList.add(options[i].value);
+                    } else {
+                        element.classList.remove(options[i].value);
+                    }
+                }
+            })
+        }
+    },
+
+    mounted() {
+        const selectors = document.querySelectorAll("[id*=select-button-]");
+        const button = document.querySelector("#dynamic-button");
+
+        selectors.forEach( (selector) => {
+            this.selectClass(selector, button);
+        });
+    }
+}
 </script>
 
 <template>
     <Banner title="About"/>
 
     <Section
-        id="section-buttons"
-        heading="Section Buttons"
+        id="section-button-dynamic"
+        heading="Button Selector"
     >
-        <div class="flex flex-col items-start gap-8 mt-8">
-            <button>
-                Base Button
-            </button>
-
-            <div class="flex gap-8">
-                <button class="button">
-                    <span class="button__text meta">Button</span>
-                </button>
-
-                <button class="button button--border">
-                    <span class="button__text meta">Border</span>
-                </button>
-            </div>
-            <div class="flex gap-8">
-                <button class="button button--thin">
-                    <span class="button__text meta">Thin</span>
-                </button>
-
-                <button class="button button--border button--thin">
-                    <span class="button__text meta">Thin</span>
-                </button>
-            </div>
-            <div class="flex gap-8">
-                <button class="button button--tall">
-                    <span class="button__text meta">Tall</span>
-                </button>
-
-                <button class="button button--border button--tall">
-                    <span class="button__text meta">Tall</span>
-                </button>
-            </div>
-
-
-            <div class="flex gap-8">
-                <button class="button button--square">
-                    <span class="button__text meta">S</span>
-                </button>
-
-                <button class="button button--square button--border">
-                    <span class="button__text meta">S</span>
-                </button>
-            </div>
-            <div class="flex gap-8">
-                <button class="button button--square button--thin">
-                    <span class="button__text meta">S</span>
-                </button>
-
-                <button class="button button--square button--border button--thin">
-                    <span class="button__text meta">S</span>
-                </button>
-            </div>
-            <div class="flex gap-8">
-                <button class="button button--square button--tall">
-                    <span class="button__text meta">S</span>
-                </button>
-
-                <button class="button button--square button--border button--tall">
-                    <span class="button__text meta">S</span>
-                </button>
+        <div class="flex mt-6 gap-4">
+            <div v-for="types, name in buttons">
+                <label :for="`select-button-${name}`" class="meta" style="font-size: 0.7rem;">
+                    {{ name }}
+                </label>
+                <select :id="`select-button-${name}`" class="mt-2" :name="name">
+                    <option value="" v-if="!types.includes('primary')" selected>
+                        none
+                    </option>
+                    <option :value="`button--${button}`" v-for="button in types">
+                        {{ button }}
+                    </option>
+                </select>
             </div>
         </div>
+
+        <div class="flex justify-center items-center p-4 mt-6 border-2" style="min-height: 240px;">
+            <button id="dynamic-button" class="button">
+                <span class="button__text meta">
+                    Button
+                </span>
+            </button>
+        </div>
+
     </Section>
 
     <Section
-        id="section-box"
-        heading="Section Box"
+        v-for="types, name in buttons"
+        :id="`section-button-${name}`"
+        :heading="`${name}`"
     >
-        <Box
-            display="row sm:col md:row"
-            align="end sm:center md:between / center sm:end"
-            class="flex-wrap mt-6 bg-offset"
-            style="min-height: 240px"
-        >
-            <div
-                v-for="index in 7"
-                :key="index"
-                class="border-face square text-primary text-6xl"
-            ></div>
-        </Box>
+        <div class="flex flex-wrap gap-8 mt-8">
+            <button :class="`button button--${button}`" :[button]="true" v-for="button in types">
+                <span class="button__text meta">
+                    {{ button }}
+                </span>
+            </button>
+        </div>
     </Section>
 </template>
---
+++ src/views/Sandbox.vue
@@ -0,0 +1,27 @@
+<script setup>
+import Banner from "@/components/Banner.vue";
+import Section from "@/components/Section.vue";
+import Box from "@/components/Box.vue";
+</script>
+
+<template>
+    <Banner title="Sandbox"/>
+
+    <Section
+        id="section-box"
+        heading="Section Box"
+    >
+        <Box
+            display="row sm:col md:row"
+            align="end sm:center md:between / center sm:end"
+            class="flex-wrap mt-6 bg-offset"
+            style="min-height: 240px"
+        >
+            <div
+                v-for="index in 7"
+                :key="index"
+                class="border-face square text-primary text-6xl"
+            ></div>
+        </Box>
+    </Section>
+</template>
