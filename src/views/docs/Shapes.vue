41e88cbf05156f5a13379348138502d3335481a4
TylerUnderwood <tylergrantunderwood@gmail.com>
2023-03-04 16:21:30 -0600

    fix: update shape docs

--- src/views/docs/Shapes.vue
+++ src/views/docs/Shapes.vue
@@ -1,60 +1,69 @@
 <script setup>
 import SectionDocs from "@/components/SectionDocs.vue";
 
-const trianglesEqual = ['t','r','b','l'];
-const trianglesRight = ['t','r','b','l'];
-const angles = ['tr','br','bl','tl'];
+const shapes = {
+    "Arch": {
+        ".Arch[data-direction=\"tr | br | bl | tl\"]": [
+            "Arch Arch--tr",
+            "Arch Arch--br",
+            "Arch Arch--bl",
+            "Arch Arch--tl",
+        ]
+    },
+    "Circle": {
+        ".Circle | .Circle .Circle--border": [
+            "Circle",
+            "Circle Circle--border",
+        ]
+    },
+    "Hexagon": {
+        ".Hexagon | .Hexagon .Hexagon--border": [
+            "Hexagon",
+            "Hexagon Hexagon--border",
+        ]
+    },
+    "Square": {
+        ".Square | .Square .Square--border": [
+            "Square",
+            "Square Square--border",
+        ]
+    },
+    "Triangle": {
+        ".Triangle .Triangle=t | r | b | l": [
+            "Triangle Triangle=t",
+            "Triangle Triangle=r",
+            "Triangle Triangle=b",
+            "Triangle Triangle=l",
+        ],
+        ".Triangle .Triangle--t | r | b | l": [
+            "Triangle Triangle--t",
+            "Triangle Triangle--r",
+            "Triangle Triangle--b",
+            "Triangle Triangle--l",
+        ],
+        ".Triangle .Triangle--tr | br | bl | tl": [
+            "Triangle Triangle--tr",
+            "Triangle Triangle--br",
+            "Triangle Triangle--bl",
+            "Triangle Triangle--tr",
+        ],
+    },
+}
 </script>
 
 <template>
-    <SectionDocs heading="Testing">
-        <div class="flex flex-col gap-8 mt-8" style="font-size: 3.6rem">
-            <div class="flex gap-4">
-                <div :class="`Arch Arch--${angle}`" v-for="angle in angles"></div>
-            </div>
-
-            <div class="flex gap-4">
-                <div class="Circle"></div>
-
-                <div class="Circle Circle--border"></div>
-            </div>
-
-            <div class="flex gap-4">
-                <div class="Square"></div>
-
-                <div class="Square Square--border"></div>
-            </div>
-
-            <div class="flex gap-4">
-                <div
-                    :class="`Triangle Triangle=${triangle}`"
-                    v-for="triangle in trianglesEqual"
-                ></div>
-            </div>
-
-            <div class="flex gap-4">
-                <div
-                    :class="`Triangle Triangle--${triangle}`"
-                    v-for="triangle in trianglesRight"
-                ></div>
-            </div>
-
-            <div class="flex gap-4">
-                <div
-                    :class="`Triangle Triangle--${triangle}`"
-                    v-for="triangle in angles"
-                ></div>
-            </div>
-
-            <div class="flex gap-4">
-                <div class="Hexagon"></div>
-
-                <div class="Hexagon Hexagon--border"></div>
-
-                <div class="Hexagon Hexagon--y"></div>
-
-                <div class="Hexagon Hexagon--y Hexagon--border"></div>
+    <SectionDocs :heading="name" v-for="(group, name) in shapes">
+        <div class="block mt-2" v-for="(shape, label) in group">
+            <code class="Code" style="font-size: 0.8rem">{{ label }}</code>
+            <div class="flex gap-4 mt-2" style="font-size: var(--shape-demo-size)">
+                <div :class="types" v-for="types in shape"></div>
             </div>
         </div>
     </SectionDocs>
 </template>
+
+<style>
+:root {
+    --shape-demo-size: 3.6rem;
+}
+</style>
