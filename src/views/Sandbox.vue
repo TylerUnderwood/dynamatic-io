6a95a6e99eb89c23a1a8f6610b53fdf5eb125bc0
TylerUnderwood <tylergrantunderwood@gmail.com>
2023-03-08 21:01:33 -0600

    fix: update section demo

--- src/views/Sandbox.vue
+++ src/views/Sandbox.vue
@@ -11,45 +11,13 @@ export default {
             sectionReverse: false,
             sectionWrap: 48,
             sectionRatio: 66,
-            sectionWrapProp: '',
-            sectionRatioProp: '',
-            sectionWrapLeftProp: '',
-            sectionWrapRightProp: '',
-            sectionWrapDiffProp: '',
             demoBlockHeight: "7rem",
+            topBlocks: 1,
+            bottomBlocks: 1,
         }
     },
 
     methods: {
-        getPropertyValueById: ( id, property ) => {
-            const section = document.getElementById(id);
-            return getComputedStyle(section).getPropertyValue(property);
-        },
-
-        trimProperty( property ) {
-            return property.replace(/\s\s+/g, ' ').trim();
-        },
-
-        updatePropertyData() {
-            const id = 'dynamic-section-column-first';
-
-            this.sectionWrapProp = this.trimProperty(
-                this.getPropertyValueById(id, '--section-wrap')
-            );
-            this.sectionRatioProp = this.trimProperty(
-                this.getPropertyValueById(id, '--section-ratio')
-            );
-            this.sectionWrapLeftProp = this.trimProperty(
-                this.getPropertyValueById(id, '--section-wrap-left')
-            );
-            this.sectionWrapRightProp = this.trimProperty(
-                this.getPropertyValueById(id, '--section-wrap-right')
-            );
-            this.sectionWrapDiffProp = this.trimProperty(
-                this.getPropertyValueById(id, '--section-wrap-diff')
-            );
-        },
-
         changeWrapperWidth() {
             document.documentElement.style.setProperty('--wrapper-page', `${this.sectionWrap}rem`);
         },
@@ -83,8 +51,6 @@ export default {
     },
 
     mounted() {
-        this.updatePropertyData();
-
         this.sectionWrap = Number(
             getComputedStyle(document.documentElement)
                 .getPropertyValue('--wrapper-page')
@@ -99,42 +65,80 @@ export default {
     <Banner title="Sandbox" intro="This is whatever I am currently working on"/>
 
     <section class="Section px-section my-section mb-0">
-        <div class="wrapper">
-            <div class="flex justify-center gap-4">
-                <label
-                    class="flex items-center gap-2"
-                    for="toggle-section-reverse"
-                >
-                    <span class="Meta" style="font-size: 0.7rem;">
-                        Reverse
-                    </span>
-                    <input
-                        id="toggle-section-reverse"
-                        name="section-reverse"
-                        type="checkbox"
-                        @click="sectionReverse = !sectionReverse"
-                    />
-                </label>
-                <label
-                    class="flex items-center gap-2"
-                    for="page-wrapper-number"
-                >
-                    <span class="Meta" style="font-size: 0.7rem;">
-                        Wrapper
-                    </span>
-                    <div>
+        <div class="Section__wrap">
+            <div class="flex justify-between gap-4">
+                <div class="block">
+                    <label
+                        class="flex items-center gap-2"
+                        for="top-blocks-number"
+                    >
+                        <div>
+                            <input
+                                id="top-blocks-number"
+                                class="Field Field--small"
+                                name="Top blocks number"
+                                type="number"
+                                min="1"
+                                max="5"
+                                v-model="topBlocks"
+                            />
+                        </div>
+                    </label>
+                </div>
+                <div class="flex gap-4">
+                    <label
+                        class="flex items-center gap-2"
+                        for="toggle-section-reverse"
+                    >
+                        <span class="Meta" style="font-size: 0.7rem;">
+                            Reverse
+                        </span>
                         <input
-                            id="page-wrapper-number"
-                            class="Field Field--small"
-                            name="Page wrapper number"
-                            type="number"
-                            min="32"
-                            max="64"
-                            v-model="sectionWrap"
-                            @input="changeWrapperWidth()"
+                            id="toggle-section-reverse"
+                            name="section-reverse"
+                            type="checkbox"
+                            @click="sectionReverse = !sectionReverse"
                         />
-                    </div>
-                </label>
+                    </label>
+                    <label
+                        class="flex items-center gap-2"
+                        for="page-wrapper-number"
+                    >
+                        <span class="Meta" style="font-size: 0.7rem;">
+                            Wrapper
+                        </span>
+                        <div>
+                            <input
+                                id="page-wrapper-number"
+                                class="Field Field--small"
+                                name="Page wrapper number"
+                                type="number"
+                                min="32"
+                                max="64"
+                                v-model="sectionWrap"
+                                @input="changeWrapperWidth()"
+                            />
+                        </div>
+                    </label>
+                </div>
+                <div class="block">
+                    <label
+                        class="flex items-center gap-2"
+                        for="bottom-blocks-number"
+                    >
+                        <div>
+                            <input
+                                id="bottom-blocks-number"
+                                class="Field Field--small"
+                                name="Bottom blocks number"
+                                type="number"
+                                min="1"
+                                max="5"
+                                v-model="bottomBlocks"
+                            />
+                        </div>
+                    </label>
+                </div>
             </div>
             <div class="flex mt-2">
                 <div class="filler w-1/4 pr-3 border-l-2 border-muted text-right">
@@ -149,7 +153,6 @@ export default {
                         max="75"
                         list="section-ratio-datalist"
                         v-model="sectionRatio"
-                        @input="updatePropertyData()"
                     />
                     <datalist
                         id="section-ratio-datalist"
@@ -176,31 +179,44 @@ export default {
         :style="`--section-ratio: ${sectionRatio / 100};`"
     >
         <div
-            id="dynamic-section-column-first"
             class="Section__column bg-stripes-secondary py-8"
             data-theme="dark"
         >
-            <div class="Section__block px-section">
+
+            <div
+                class="Section__block px-section"
+                :class="{'mt-4':index!=0}"
+                v-for="(block, index) in topBlocks"
+            >
                 <div class="Section__wrap px-section">
                     <DemoBlock :height="demoBlockHeight">
-                        <span class="Meta">Demo Block First</span>
+                        <span class="Meta">Demo Block Top</span>
                     </DemoBlock>
                 </div>
             </div>
+
         </div>
-        <div class="Section__column mt-8 md:mt-0 md:py-8">
-            <div class="Section__block px-section">
+        <div
+            class="Section__column mt-8 md:mt-0 md:py-8"
+        >
+
+            <div
+                class="Section__block px-section"
+                :class="{'mt-4':index!=0}"
+                v-for="(block, index) in bottomBlocks"
+            >
                 <div class="Section__wrap px-section">
                     <DemoBlock :height="demoBlockHeight">
-                        <span class="Meta">Demo Block Last</span>
+                        <span class="Meta">Demo Block Bottom</span>
                     </DemoBlock>
                 </div>
             </div>
+
         </div>
     </section>
 
-    <section class="px-section my-section mt-0">
-        <div class="wrapper">
+    <section class="Section px-section my-section mt-0">
+        <div class="Section__wrap">
             <div class="flex">
                 <div :style="`width: ${sectionRatio}%`">
                     <div class="bg-offset border-x-2 border-muted text-center p-1">
