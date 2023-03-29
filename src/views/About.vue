2f71747656584fee95d814627eef118212ba6baf
TylerUnderwood <tylergrantunderwood@gmail.com>
2023-03-28 20:36:24 -0500

    fix: move content

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,69 +1,38 @@
 <script setup>
 import Banner from "@/components/Banner.vue";
 import Section from "@/components/Section.vue";
-import SectionSplit from "@/components/SectionSplit.vue";
-import SectionBlock from "@/components/SectionBlock.vue";
-
-const topItems = [
-    {
-        copy: 'Hello',
-    },
-    {
-        copy: 'Hello',
-        blockClass: 'bg-base theme-body py-4 mt-8 md:mt-20',
-    },
-    {
-        copy: 'Hello',
-    },
-];
-
-const botItems = [
-    {
-        copy: 'Hello',
-    },
-    {
-        copy: 'Hello',
-    },
-    {
-        copy: 'Hello',
-        blockClass: 'mt-8 md:mt-20',
-    },
-];
-
 </script>
 
 <template>
     <Banner title="About"/>
 
-    <Section heading="Something" wrapWidth="var(--max-w-copy)">
+    <Section heading="Something">
         <p class="mt-6">Hello, here is some text.</p>
     </Section>
 
-    <SectionSplit
-        classTop="bg-primary theme-dark py-8"
-        classBot="mt-8 md:mt-0 justify-center"
-        :ratio="66"
-    >
-        <template #top>
-            <SectionBlock
-                class="Markup Rhythm"
-                :class="item.blockClass ? item.blockClass : 'mt-8'"
-                v-for="item, index in topItems"
-                :wrapWidth="index === 1 ? 'var(--max-w-copy)' : null"
-            >
-                <h3>Top {{ index + 1 }}</h3>
-                <p>{{ item.copy }}</p>
-            </SectionBlock>
-        </template>
-        <template #bot>
-            <SectionBlock
-                class="Markup Rhythm"
-                :class="item.blockClass ? item.blockClass : 'mt-8'"
-                v-for="item, index in botItems"
-            >
-                <h3>Bottom {{ index + 1 }}</h3>
-                <p>{{ item.copy }}</p>
-            </SectionBlock>
-        </template>
-    </SectionSplit>
+    <Section>
+        <div class="theme-dark bg-primary p-20 px-8 md:px-20">
+            <span class="Heading">Theme Dark</span>
+            <code class="Code mt-2" style="font-size: 0.8rem">.theme-dark .bg-primary</code>
+            <p class="Copy mt-2">This container will all ways be dark.</p>
+            <ul class="List List--bullet List--colorFace mt-4">
+                <li class="List__item" v-for="item in 4">Item {{ item }}</li>
+            </ul>
+            <div class="theme-body bg-base mt-6 p-20 px-8 md:px-20">
+                <span class="Heading">Theme Body</span>
+                <code class="Code mt-2" style="font-size: 0.8rem">.theme-body .bg-base</code>
+                <p class="Copy mt-2">This container will follow whatever the <code class="Code">body</code> tag uses.</p>
+                <ul class="List List--bullet mt-4">
+                    <li class="List__item" v-for="item in 4">Item {{ item }}</li>
+                </ul>
+            </div>
+        </div>
+    </Section>
 </template>
+
+<style>
+/* .theme-dark.bg-primary .List > .List__item::before, */
+.List--colorFace > .List__item::before {
+    color: var(--theme-face);
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,27 +1,84 @@
 <script setup>
 import Banner from "@/components/Banner.vue";
 import Section from "@/components/Section.vue";
+import SectionSplit from "@/components/SectionSplit.vue";
+import SectionBlock from "@/components/SectionBlock.vue";
+
+const topItems = [
+    {
+        copy: 'Hello',
+    },
+    {
+        copy: 'Hello',
+        blockClass: 'bg-base theme-body py-4 mt-8 md:mt-20',
+    },
+    {
+        copy: 'Hello',
+    },
+];
+
+const botItems = [
+    {
+        copy: 'Hello',
+    },
+    {
+        copy: 'Hello',
+    },
+    {
+        copy: 'Hello',
+        blockClass: 'mt-8 md:mt-20',
+    },
+];
 </script>
 
 <template>
-    <Banner title="Home" intro="Welcome to Dynamatic" />
+    <Banner
+        title="Home"
+        intro="Welcome to Dynamatic"
+    />
 
-    <section class="Section px-page my-section">
-        <div class="page-wrapper">
-            <div class="flex flex-col md:flex-row gap-6">
-                <div class="sm:w-1/2">
-                    <router-link to="/sandbox" class="Button" data-size="small">
-                        <span class="Button__text Meta">Sandbox</span>
-                    </router-link>
-                    <p class="Copy mt-2">Head over to my sandbox to see what I am currently working on.</p>
-                </div>
-                <div class="sm:w-1/2">
-                    <router-link to="/docs" class="Button" data-size="small">
-                        <span class="Button__text Meta">Docs</span>
-                    </router-link>
-                    <p class="Copy mt-2">Or checkout the Docs to see some more "finalized" elements.</p>
-                </div>
+    <Section>
+        <div class="flex flex-col md:flex-row gap-6">
+            <div class="sm:w-1/2">
+                <router-link to="/sandbox" class="Button" data-size="small">
+                    <span class="Button__text Meta">Sandbox</span>
+                </router-link>
+                <p class="Copy mt-2">Head over to my sandbox to see what I am currently working on.</p>
+            </div>
+            <div class="sm:w-1/2">
+                <router-link to="/docs" class="Button" data-size="small">
+                    <span class="Button__text Meta">Docs</span>
+                </router-link>
+                <p class="Copy mt-2">Or checkout the Docs to see some more "finalized" elements.</p>
             </div>
         </div>
-    </section>
+    </Section>
+
+    <SectionSplit
+        classTop="bg-primary theme-dark py-8"
+        classBot="mt-8 md:mt-0 justify-center"
+        :ratio="66"
+    >
+        <template #top>
+            <SectionBlock
+                class="Markup Rhythm"
+                :class="item.blockClass ? item.blockClass : 'mt-8'"
+                v-for="item, index in topItems"
+                :wrapWidth="index === 1 ? 'var(--max-w-copy)' : null"
+            >
+                <h3>Top {{ index + 1 }}</h3>
+                <p>{{ item.copy }}</p>
+            </SectionBlock>
+        </template>
+        <template #bot>
+            <SectionBlock
+                class="Markup Rhythm"
+                :class="item.blockClass ? item.blockClass : 'mt-8'"
+                v-for="item, index in botItems"
+            >
+                <h3>Bottom {{ index + 1 }}</h3>
+                <p>{{ item.copy }}</p>
+            </SectionBlock>
+        </template>
+    </SectionSplit>
 </template>
