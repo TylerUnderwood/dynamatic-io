00645591388a50a83601a07856d85bec4cf9e63e
TylerUnderwood <tylergrantunderwood@gmail.com>
2023-04-15 17:10:58 -0500

    fix: move the settings-panel

--- src/components/Settings.vue
+++ src/components/Settings.vue
@@ -1,121 +1,146 @@
 <script>
 export default {
-  methods: {
-    setSwitch() {
-      const toggle = document.querySelector('#toggle-scheme');
+    methods: {
+        setSwitch() {
+            const toggle = document.querySelector('#toggle-scheme');
 
-      const watchPreferred = () => {
-        if (!window.matchMedia) return;
-        const prefersDark = window.matchMedia('(prefers-color-scheme: dark)');
+            const watchPreferred = () => {
+                if (!window.matchMedia) return;
+                const prefersDark = window.matchMedia('(prefers-color-scheme: dark)');
 
-        if (prefersDark.matches) {
-          toggle.checked = true;
-        } else {
-          toggle.checked = false;
-        };
+                if (prefersDark.matches) {
+                    toggle.checked = true;
+                } else {
+                    toggle.checked = false;
+                };
 
-        prefersDark.addEventListener('change', event => {
-          if (event.matches) {
-            toggle.checked = true;
-          } else {
-            toggle.checked = false;
-          };
-        });
-      }
+                prefersDark.addEventListener('change', event => {
+                    if (event.matches) {
+                        toggle.checked = true;
+                    } else {
+                        toggle.checked = false;
+                    };
+                });
+            }
 
-      const setSwitch = (scheme) => {
-        switch(scheme){
-          case 'dark':
-            toggle.checked = true;
-            break;
-          case 'light':
-            toggle.checked = false;
-            break;
-          case 'normal':
-            toggle.checked = false;
-            break;
-          default:
-            watchPreferred();
-            break;
+            const setSwitch = (scheme) => {
+                switch(scheme){
+                    case 'dark':
+                        toggle.checked = true;
+                        break;
+                    case 'light':
+                        toggle.checked = false;
+                        break;
+                    case 'normal':
+                        toggle.checked = false;
+                        break;
+                    default:
+                        watchPreferred();
+                        break;
+                }
+            }
+
+            setSwitch(document.body.dataset.scheme);
+        },
+
+        toggleScheme() {
+            const toggle = document.querySelector('#toggle-scheme');
+
+            toggle.addEventListener('change', (event) => {
+                if (event.currentTarget.checked) {
+                    document.body.dataset.scheme = "dark";
+                } else {
+                    document.body.dataset.scheme = "light";
+                }
+            });
+        },
+
+        toggleBodyAttrValue( id, attr, value ) {
+            const toggle = document.querySelector(`#${id}`);
+
+            if (document.body.getAttribute(attr) === null) {
+                document.body.setAttribute(attr, "")
+            }
+
+            if (document.body.getAttribute(attr).includes(value)) {
+                toggle.checked = true;
+            }
+
+            toggle.addEventListener('change', (event) => {
+                const currentValues = document.body.getAttribute(attr);
+                const hasValue = currentValues.includes(value);
+                let newValues = currentValues.split(' ');
+
+                if (event.currentTarget.checked) {
+                    if (!hasValue) newValues.push(value);
+                    console.log(`Add: ${attr} "${value}"`);
+                } else {
+                    if (hasValue) newValues = newValues.filter(item => item !== value);
+                    console.log(`Remove: ${attr} "${value}"`);
+                }
+
+                document.body.setAttribute(attr, newValues.join(' '));
+            })
         }
-      }
-
-      setSwitch(document.body.dataset.scheme);
     },
 
-    toggleScheme() {
-      const toggle = document.querySelector('#toggle-scheme');
-
-      toggle.addEventListener('change', (event) => {
-        if (event.currentTarget.checked) {
-          document.body.dataset.scheme = "dark";
-        } else {
-          document.body.dataset.scheme = "light";
-        }
-      });
-    },
-
-    toggleBodyAttrValue( id, attr, value ) {
-      const toggle = document.querySelector(`#${id}`);
-
-      if (document.body.getAttribute(attr) === null) {
-        document.body.setAttribute(attr, "")
-      }
-
-      if (document.body.getAttribute(attr).includes(value)) {
-        toggle.checked = true;
-      }
-
-      toggle.addEventListener('change', (event) => {
-        const currentValues = document.body.getAttribute(attr);
-        const hasValue = currentValues.includes(value);
-        let newValues = currentValues.split(' ');
-
-        if (event.currentTarget.checked) {
-          if (!hasValue) newValues.push(value);
-          console.log(`Add: ${attr} "${value}"`);
-        } else {
-          if (hasValue) newValues = newValues.filter(item => item !== value);
-          console.log(`Remove: ${attr} "${value}"`);
-        }
-
-        document.body.setAttribute(attr, newValues.join(' '));
-      })
+    mounted() {
+        this.setSwitch();
+        this.toggleScheme();
+        this.toggleBodyAttrValue('toggle-identify-guidelines', 'identify', 'guidelines');
+        // this.toggleBodyAttrValue('toggle-identify-components', 'identify', 'components');
     }
-  },
-
-  mounted() {
-    this.setSwitch();
-    this.toggleScheme();
-    this.toggleBodyAttrValue('toggle-identify-guidelines', 'identify', 'guidelines');
-    this.toggleBodyAttrValue('toggle-identify-components', 'identify', 'components');
-  }
 }
 </script>
 
 <template>
-  <div id="settings" class="settings-tab p-1 bg-offset border-contrast">
-    <input type="checkbox" name="Toggle Scheme" id="toggle-scheme"/>
-    <div class="pt-2"></div>
-    <input type="checkbox" name="Toggle Guidelines" id="toggle-identify-guidelines"/>
-    <div class="pt-2"></div>
-    <input type="checkbox" name="Toggle Identify Components" id="toggle-identify-components"/>
-  </div>
+    <div id="settings" class="SettingsConsole">
+        <!--
+        <label for="select-theme">
+            <select
+                id="select-theme"
+                name="Select Theme"
+            >
+                <option value="auto">Auto</option>
+                <option value="dark">Dark</option>
+                <option value="light">Light</option>
+            </select>
+        </label>
+        -->
+        <input
+            type="checkbox"
+            id="toggle-scheme"
+            name="Toggle Scheme"
+        />
+        <input
+            type="checkbox"
+            id="toggle-identify-guidelines"
+            name="Toggle Guidelines"
+        />
+        <!--
+        <input
+            type="checkbox"
+            id="toggle-identify-components"
+            name="Toggle Identify Components"
+        />
+        -->
+    </div>
 </template>
 
 <style>
-.settings-tab {
+.SettingsConsole {
     position: fixed;
-    top: 50%;
-    right: unset;
-    bottom: unset;
-    left: 100%;
-    translate: -100% -50%;
+    top: unset;
+    right: 0;
+    bottom: 0;
+    left: unset;
     display: flex;
-    flex-direction: column;
     border-width: 2px;
-    border-style: solid;
-    border-right-width: 0;
+    border-bottom-width: 0;
     border-radius: 3px;
+    background-color: var(--theme-offset);
+    padding: 0.5em;
+    gap: 0.5em;
+    font-size: 0.7rem;
 }
 </style>
