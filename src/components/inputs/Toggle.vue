a836c71b68631c34f9f5710a89b7fcaa74db6cf5
Tyler Underwood <tylergrantunderwood@gmail.com>
2026-07-13 13:39:03 -0500

    feat: wip adding color gradient to toggle switch

--- src/components/inputs/Toggle.vue
+++ src/components/inputs/Toggle.vue
@@ -34,4 +34,41 @@ const model = defineModel()
 
 <style>
 @import "@styles/components/Toggle.css";
+
+.Toggle__switch {
+    border-width: 0;
+}
+.Toggle__switch::before {
+    inset: 0;
+    background-repeat: no-repeat;
+    background-image:
+        linear-gradient(
+            to right,
+            var(--theme-secondary) 0%,
+            var(--theme-primary) 50%,
+            var(--theme-primary) 100%
+        ),
+        linear-gradient(
+            to left,
+            var(--theme-secondary) 0%,
+            var(--theme-primary) 50%,
+            var(--theme-primary) 100%
+        );
+    background-position: left, right;
+    background-size: 0% 100%, 100% 100%;
+    transition-property: background;
+}
+.Toggle__switch::after {
+    transform: unset;
+    translate: 0;
+    margin: var(--toggle-border-width);
+    width: unset;
+}
+input:checked + .Toggle__switch::before {
+    background-size: 100% 100%, 0% 100%;
+}
+input:checked + .Toggle__switch::after {
+    transform: unset;
+    translate: calc(-100% - (var(--toggle-border-width) * 2));
+}
 </style>
